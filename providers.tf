39e0bab736572688ca5c370d291b0c3f378c83ad
Hovo-888 <155750541+Hovo-888@users.noreply.github.com>
2024-01-25 23:57:08 +0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,10 @@
-# AWS provider
 provider "aws" {
   region = "us-east-1"
 }
 
 data "aws_caller_identity" "current" {}
 
-// http providery ogtagorcumenq nra hamar vor SSH anenq mer IP ic vor mer IP dynamic vercni
+
 terraform {
   # http provider
   required_providers {
