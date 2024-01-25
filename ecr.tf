49e5b87b7381bdda7746ba5ab9491cbabdb94682
Hovo-888 <155750541+Hovo-888@users.noreply.github.com>
2024-01-26 00:15:43 +0400

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,3 @@
-// Stexcuma AWS Elastic Container Registry vor docker imagnery pushenq aydtex
 resource "aws_ecrpublic_repository" "task_app_repo" {
   repository_name      = var.ecr_vars.repo_name
   force_destroy = true
