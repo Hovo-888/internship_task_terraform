84f5111c8406aa3075fba0a0e113552f1b8c67b3
Hovo-888 <155750541+Hovo-888@users.noreply.github.com>
2024-01-25 23:49:24 +0400

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,4 +1,3 @@
-// Stexcuma public subnet VPC i hamar 1
 resource "aws_subnet" "task_public_subnet_1" {
   vpc_id                  = aws_vpc.task-vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -10,7 +9,6 @@ resource "aws_subnet" "task_public_subnet_1" {
   }
 }
 
-// Stexcuma public subnet VPC i hamar 2
 resource "aws_subnet" "task_public_subnet_2" {
   vpc_id                  = aws_vpc.task-vpc.id
   cidr_block              = "10.0.3.0/24"
@@ -22,7 +20,6 @@ resource "aws_subnet" "task_public_subnet_2" {
   }
 }
 
-// Stexcuma private subnet VPC i hamar
 resource "aws_subnet" "task_private_subnet" {
   vpc_id     = aws_vpc.task-vpc.id
   cidr_block = "10.0.2.0/24"
